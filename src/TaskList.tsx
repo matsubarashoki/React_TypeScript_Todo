dc4157340bbbfede613bfbfb4d862131c4f348ee
matsubarashoki <matsubarashoki@gmail.com>
2023-09-24 18:17:40 +0900

    Update func

--- src/TaskList.tsx
+++ src/TaskList.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import type { todo } from "./Todo";
 
 type TaskListProps = {
@@ -7,23 +7,71 @@ type TaskListProps = {
 };
 
 const TaskList: React.FC<TaskListProps> = ({ todos, setTodos }) => {
+  const [inputValue, setInputValue] = useState("");
+
   const completeTodo = (todo: todo) => {
     const newTodos = todos.filter((_todo) => {
       return _todo.id !== todo.id;
     });
     setTodos(newTodos);
   };
+  const toggleEditMode = (_todo: todo) => {
+    setInputValue(_todo.text);
+    const newTodos2 = todos.map((todo) =>
+      todo.id === _todo.id ? { ...todo, isEditing: true } : todo
+    );
+    return setTodos(newTodos2);
+  };
+  const updateTodo = (todo: todo) => {
+    console.log(todo);
+    console.log(inputValue);
+    const newTodo3: todo = {
+      ...todo,
+      isEditing: !todo.isEditing,
+      text: inputValue,
+    };
+    console.log(typeof todo.id);
+    console.log(typeof newTodo3.id);
+
+    const newTodos3 = todos.map((todo) =>
+      todo.id === newTodo3.id ? newTodo3 : todo
+    );
+    setTodos(newTodos3);
+    console.log(todos);
+    return;
+  };
+  const changeInputHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    // console.log(e.target.value)
+    setInputValue(e.target.value);
+  };
+
   return (
-    <ul>
+    <>
       {todos.map((todo) => {
         return (
           <div key={todo.id}>
-            <li>{todo.text}</li>
-            <button onClick={() => completeTodo(todo)}>完了</button>
+            {/* <form style={{ display: "inline" }}> */}
+            {todo.isEditing ? (
+              <input
+                type="text"
+                defaultValue={inputValue}
+                onChange={(e) => changeInputHandler(e)}
+              />
+            ) : (
+              <span onDoubleClick={() => toggleEditMode(todo)}>
+                {todo.text}
+              </span>
+            )}
+            {todo.isEditing ? (
+              <button onClick={() => updateTodo(todo)}>更新</button>
+            ) : (
+              <button onClick={() => completeTodo(todo)}>完了</button>
+            )}
+            {/* </form> */}
           </div>
         );
       })}
-    </ul>
+    </>
   );
 };
 
--- src/Todo.tsx
+++ src/Todo.tsx
@@ -4,19 +4,23 @@ import TaskList from "./TaskList";
 export type todo = {
   id: number;
   text: string;
+  isEditing: boolean;
 };
 
 const TodoComporment = () => {
   const [inputText, setInputText] = useState("");
   const [todos, setTodos] = useState<todo[]>([
-    { id: 0, text: "Todo1" },
-    { id: 1, text: "Todo2" },
+    { id: 0, text: "Todo1", isEditing: false },
+    { id: 1, text: "Todo2", isEditing: false },
   ]);
 
   const changeHandler = (e: React.ChangeEvent<HTMLInputElement>) =>
     setInputText(e.target.value);
   const addTodoItem = () => {
-    setTodos((todos) => [...todos, { id: todos.length, text: inputText }]);
+    setTodos((todos) => [
+      ...todos,
+      { id: todos.length, text: inputText, isEditing: false },
+    ]);
     setInputText("");
   };
   return (
